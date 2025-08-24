48223413758a663f3a248557fdf971ad64f0aede
eka risyana <risyana@gmail.com>
2025-08-24 18:47:03 +0700

    add cache for 'fetch'

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,5 +1,5 @@
 export default async function Page() {
-    const data = await fetch('https://api.vercel.app/blog')
+    const data = await fetch('https://api.vercel.app/blog', {cache: 'force-cache' })
     const posts = await data.json()
     return (
         <>

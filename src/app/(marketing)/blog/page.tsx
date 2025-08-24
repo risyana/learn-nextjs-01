640a66f4264ab3782f4d859feec135bd16647880
eka risyana <risyana@gmail.com>
2025-08-24 19:02:33 +0700

    data fetching using 'use' hook and component level Suspense

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,18 +1,18 @@
-export default async function Page() {
-    const data = await fetch('https://api.vercel.app/blog', {cache: 'force-cache' })
-    const posts = await data.json()
+import Posts from "@/components/posts";
+import { Suspense } from "react";
+
+export default function Page() {
+    const getPosts = async() => {
+        const data = await fetch('https://api.vercel.app/blog')
+        const posts = await data.json()
+        return posts
+    }
+
+    const posts = getPosts()
+
     return (
-        <>
-            <h3>Blog content</h3>
-            <ol>
-                {
-                    posts.map((post: { id: number; title: string }) => (
-                        <li key={post.id}>
-                            <p>{post.title}</p>
-                        </li>
-                    ))
-                }
-            </ol>
-        </>
+        <Suspense fallback={<p>Loading data...</p>}>
+            <Posts posts={posts} />
+        </Suspense>
     )
 }

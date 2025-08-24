0fc0a3bbbb726981eb7fb59efac9f43e5e86f4d7
eka risyana <risyana@gmail.com>
2025-08-24 18:42:30 +0700

    fething data using 'fetch'

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,5 +1,18 @@
-export default function Blog() {
+export default async function Page() {
+    const data = await fetch('https://api.vercel.app/blog')
+    const posts = await data.json()
     return (
-        <p>Blog content</p>
+        <>
+            <h3>Blog content</h3>
+            <ol>
+                {
+                    posts.map((post: { id: number; title: string }) => (
+                        <li key={post.id}>
+                            <p>{post.title}</p>
+                        </li>
+                    ))
+                }
+            </ol>
+        </>
     )
 }
